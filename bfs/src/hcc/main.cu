c44b0b00713fffe6b964bfbe1e9a735d490dc0c0
UncleHandsome <yunwei@multicorewareinc.com>
2016-07-29 02:30:41 -0500

    [BFS] Update main file

--- bfs/src/hcc/main.cu
+++ bfs/src/hcc/main.cu
@@ -31,13 +31,21 @@
 #include <parboil.h>
 #include <deque>
 #include <iostream>
+#include <hc.hpp>
 #include "config.h"
+
 FILE *fp;
+struct Node {
+  int x;
+  int y;
+};
+struct Edge {
+  int x;
+  int y;
+};
 
-typedef int2 Node;
-typedef int2 Edge;
 
-#include "kernel.cu"
+#include "kernel.cpp"
 const int h_top = 1;
 const int zero = 0;
 
@@ -110,79 +118,52 @@ int main(int argc, char** argv)
 
   //Copy the Node list to device memory
   //Copy the Node list to device memory
-  Node* d_graph_nodes;
-  cudaMalloc((void**) &d_graph_nodes, sizeof(Node)*num_of_nodes);
-  cudaMemcpy(d_graph_nodes, h_graph_nodes, sizeof(Node)*num_of_nodes, cudaMemcpyHostToDevice);
+  array_view<Node*> d_graph_nodes(num_of_nodes, h_graph_nodes);
   //Copy the Edge List to device Memory
-  Edge* d_graph_edges;
-  cudaMalloc((void**) &d_graph_edges, sizeof(Edge)*num_of_edges);
-  cudaMemcpy(d_graph_edges, h_graph_edges, sizeof(Edge)*num_of_edges, cudaMemcpyHostToDevice);
+  array_view<Edge*> d_graph_edges(num_of_edges, h_graph_edges);
 
-  int* d_color;
-  cudaMalloc((void**) &d_color, sizeof(int)*num_of_nodes);
-  int* d_cost;
-  cudaMalloc((void**) &d_cost, sizeof(int)*num_of_nodes);
-  int * d_q1;
-  int * d_q2;
-  cudaMalloc((void**) &d_q1, sizeof(int)*num_of_nodes);
-  cudaMalloc((void**) &d_q2, sizeof(int)*num_of_nodes);
-  int * tail;
-  cudaMalloc((void**) &tail, sizeof(int));
-  int *front_cost_d;
-  cudaMalloc((void**) &front_cost_d, sizeof(int));
-  cudaMemcpy(d_color, color, sizeof(int)*num_of_nodes, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_cost, h_cost, sizeof(int)*num_of_nodes, cudaMemcpyHostToDevice);
-
-  //bind the texture memory with global memory
-  cudaBindTexture(0,g_graph_node_ref,d_graph_nodes, sizeof(Node)*num_of_nodes);
-  cudaBindTexture(0,g_graph_edge_ref,d_graph_edges,sizeof(Edge)*num_of_edges);
-
+  array_view<int> d_color(num_of_nodes, color);
+  array_view<int> d_cost(num_of_nodes, h_cost);
+  array_view<int> d_q1(num_of_nodes);
+  array_view<int> d_q2(num_of_nodes);
+  array_view<int> tail(1);
+  array_view<int> front_cost_d(1);
 
   printf("Starting GPU kernel\n");
-  cudaThreadSynchronize();
   pb_SwitchToTimer(&timers, pb_TimerID_KERNEL);
 
   int num_of_blocks;
   int num_of_threads_per_block;
 
-  cudaMemcpy(tail,&h_top,sizeof(int),cudaMemcpyHostToDevice);
-  cudaMemcpy(&d_cost[source],&zero,sizeof(int),cudaMemcpyHostToDevice);
+  tail[0] = h_top;
+  d_cost[source] = zero;
+  d_q1[0] = source;
 
-  cudaMemcpy( &d_q1[0], &source, sizeof(int), cudaMemcpyHostToDevice);
   int num_t;//number of threads
   int k=0;//BFS level index
 
   //whether or not to adjust "k", see comment on "BFS_kernel_multi_blk_inGPU" for more details
-  int * switch_kd;
-  cudaMalloc((void**) &switch_kd, sizeof(int));
-  int * num_td;//number of threads
-  cudaMalloc((void**) &num_td, sizeof(int));
+  array_view<int> switch_kd(1);
+  array_view<int> num_td(1);
 
   //whether to stay within a kernel, used in "BFS_kernel_multi_blk_inGPU"
-  bool *stay;
-  cudaMalloc( (void**) &stay, sizeof(bool));
+  array_view<int> stay(1);
   int switch_k;
 
   //max number of frontier nodes assigned to a block
-  int * max_nodes_per_block_d;
-  cudaMalloc( (void**) &max_nodes_per_block_d, sizeof(int));
-  int *global_kt_d;
-  cudaMalloc( (void**) &global_kt_d, sizeof(int));
-  cudaMemcpy(global_kt_d,&zero, sizeof(int),cudaMemcpyHostToDevice);
+  array_view<int> max_nodes_per_block_d(1);
+  array_view<int> global_kt_d(1);
+  global_kt_d[0] = zero;
 
   int h_overflow = 0;
-  int *d_overflow;
-  cudaMalloc((void**) &d_overflow, sizeof(int));
-  cudaMemcpy(d_overflow, &h_overflow, sizeof(int), cudaMemcpyHostToDevice);
+  array_view<int> d_overflow(1);
+  d_overflow[0] = h_overflow;
   do
   {
-    cudaMemcpy( &num_t, tail, sizeof(int), cudaMemcpyDeviceToHost);
-    cudaMemcpy(tail,&zero,sizeof(int),cudaMemcpyHostToDevice);
+    num_t[0] = tail;
+    tail[0] = zero;
 
     if(num_t == 0){//frontier is empty
-      cudaFree(stay);
-      cudaFree(switch_kd);
-      cudaFree(num_td);
       break;
     }
 
@@ -201,78 +182,81 @@ int main(int argc, char** argv)
       num_of_blocks = NUM_SM;
 
     //assume "num_of_blocks" can not be very large
-    dim3  grid( num_of_blocks, 1, 1);
-    dim3  threads( num_of_threads_per_block, 1, 1);
+    extent<1>  grid(num_of_blocks * num_of_threads_per_block);
+    tiled_extent<1>  tile = grid.tile(num_of_threads_per_block);
 
     if(k%2 == 0){
       if(num_of_blocks == 1){
-        BFS_in_GPU_kernel<<< grid, threads >>>(d_q1,d_q2, d_graph_nodes,
-            d_graph_edges, d_color, d_cost,num_t , tail,GRAY0,k,d_overflow);
+        parallel_for_each(tile, [=] (tiled_index<1> tidx) [[hc]]
+                {
+                    BFS_in_GPU_kernel(tidx, d_q1,d_q2, d_graph_nodes,
+                    d_graph_edges, d_color, d_cost,num_t , tail,GRAY0,k,d_overflow);
+                });
       }
       else if(num_of_blocks <= NUM_SM){
-        (cudaMemcpy(num_td,&num_t,sizeof(int),
-                    cudaMemcpyHostToDevice));
-        BFS_kernel_multi_blk_inGPU
-          <<< grid, threads >>>(d_q1,d_q2, d_graph_nodes,
-              d_graph_edges, d_color, d_cost, num_td, tail,GRAY0,k,
-              switch_kd, max_nodes_per_block_d, global_kt_d,d_overflow);
-        (cudaMemcpy(&switch_k,switch_kd, sizeof(int),
-                    cudaMemcpyDeviceToHost));
+          num_td[0] = num_t
+              parallel_for_each(tile, [=] (tiled_index<1> tidx) [[hc]]
+                      {
+                      BFS_kernel_multi_blk_inGPU (tile, d_q1,d_q2, d_graph_nodes,
+                          d_graph_edges, d_color, d_cost, num_td, tail,GRAY0,k,
+                          switch_kd, max_nodes_per_block_d, global_kt_d,d_overflow);
+                      });
+          switch_k = switch_kd[0];
         if(!switch_k){
           k--;
         }
       }
       else{
-        BFS_kernel<<< grid, threads >>>(d_q1,d_q2, d_graph_nodes,
-            d_graph_edges, d_color, d_cost, num_t, tail,GRAY0,k,d_overflow);
+          parallel_for_each(tile, [=] (tiled_index<1> tidx) [[hc]]
+                  {
+                  BFS_kernel(tile, d_q1,d_q2, d_graph_nodes,
+                      d_graph_edges, d_color, d_cost, num_t, tail,GRAY0,k,d_overflow);
+                  });
       }
     }
     else{
       if(num_of_blocks == 1){
-        BFS_in_GPU_kernel<<< grid, threads >>>(d_q2,d_q1, d_graph_nodes,
-            d_graph_edges, d_color, d_cost, num_t, tail,GRAY1,k,d_overflow);
+          parallel_for_each(tile, [=] (tiled_index<1> tidx) [[hc]]
+                  {
+                  BFS_in_GPU_kernel(tile, d_q2,d_q1, d_graph_nodes,
+                      d_graph_edges, d_color, d_cost, num_t, tail,GRAY1,k,d_overflow);
+                  });
       }
       else if(num_of_blocks <= NUM_SM){
-        (cudaMemcpy(num_td,&num_t,sizeof(int),
-                    cudaMemcpyHostToDevice));
-        BFS_kernel_multi_blk_inGPU
-          <<< grid, threads >>>(d_q2,d_q1, d_graph_nodes,
-              d_graph_edges, d_color, d_cost, num_td, tail,GRAY1,k,
-              switch_kd, max_nodes_per_block_d, global_kt_d,d_overflow);
-        (cudaMemcpy(&switch_k,switch_kd, sizeof(int),
-                    cudaMemcpyDeviceToHost));
+          num_td[0] = num_t;
+          parallel_for_each(tile, [=] (tiled_index<1> tidx) [[hc]]
+                  {
+                  BFS_kernel_multi_blk_inGPU(tile, d_q2,d_q1, d_graph_nodes,
+                      d_graph_edges, d_color, d_cost, num_td, tail,GRAY1,k,
+                      switch_kd, max_nodes_per_block_d, global_kt_d,d_overflow);
+                  });
+          switch_k = switch_kd[0];
         if(!switch_k){
           k--;
         }
       }
       else{
-        BFS_kernel<<< grid, threads >>>(d_q2,d_q1, d_graph_nodes,
-            d_graph_edges, d_color, d_cost, num_t, tail, GRAY1,k,d_overflow);
+          parallel_for_each(tile, [=] (tiled_index<1> tidx) [[hc]]
+                  {
+                  BFS_kernel(tile, d_q2,d_q1, d_graph_nodes,
+                      d_graph_edges, d_color, d_cost, num_t, tail, GRAY1,k,d_overflow);
+                  });
       }
     }
     k++;
-    cudaMemcpy(&h_overflow, d_overflow, sizeof(int), cudaMemcpyDeviceToHost);
+    h_overflow = d_overflow[0];
     if(h_overflow) {
       printf("Error: local queue was overflown. Need to increase W_LOCAL_QUEUE\n");
       return 0;
     }
   } while(1);
-  cudaThreadSynchronize();
   pb_SwitchToTimer(&timers, pb_TimerID_COPY);
   printf("GPU kernel done\n");
 
   // copy result from device to host
-  cudaMemcpy(h_cost, d_cost, sizeof(int)*num_of_nodes, cudaMemcpyDeviceToHost);
-  cudaMemcpy(color, d_color, sizeof(int)*num_of_nodes, cudaMemcpyDeviceToHost);
-  cudaUnbindTexture(g_graph_node_ref);
-  cudaUnbindTexture(g_graph_edge_ref);
+  h_cost.synchronize();
+  color.synchronize();
 
-  cudaFree(d_graph_nodes);
-  cudaFree(d_graph_edges);
-  cudaFree(d_color);
-  cudaFree(d_cost);
-  cudaFree(tail);
-  cudaFree(front_cost_d);
   //Store the result into a file
   pb_SwitchToTimer(&timers, pb_TimerID_IO);
   FILE *fp = fopen(params->outFile,"w");
@@ -282,10 +266,6 @@ int main(int argc, char** argv)
   fclose(fp);
 
   // cleanup memory
-  free( h_graph_nodes);
-  free( h_graph_edges);
-  free( color);
-  free( h_cost);
   pb_SwitchToTimer(&timers, pb_TimerID_NONE);
   pb_PrintTimerSet(&timers);
   pb_FreeParameters(params);

110c83cafc7aca70613fe60206c7ba14ead24918
UncleHandsome <yunwei@multicorewareinc.com>
2016-08-01 03:52:59 -0500

    [STENCIL] Finish

--- benchmarks/stencil/src/hcc/kernels.hpp
+++ benchmarks/stencil/src/hcc/kernels.hpp
@@ -9,29 +9,36 @@
 #include "common.h"
 
 
-__global__ void block2D_hybrid_coarsen_x(float c0,float c1,float *A0,float *Anext, int nx, int ny, int nz)
+void block2D_hybrid_coarsen_x(
+        tiled_index<3>& tidx,
+        float c0,float c1,
+        const array_view<float>& A0, const array_view<float>& Anext,
+        int nx, int ny, int nz) [[hc]]
 {
+    index<3> threadIdx(tidx.local);
+    index<3> blockIdx(tidx.tile);
+    index<3> blockDim(tidx.tile_dim);
 
 	//thread coarsening along x direction
-	const int i = blockIdx.x*blockDim.x*2+threadIdx.x;
-	const int i2= blockIdx.x*blockDim.x*2+threadIdx.x+blockDim.x;
-    const int j = blockIdx.y*blockDim.y+threadIdx.y;
-	const int sh_id=threadIdx.x + threadIdx.y*blockDim.x*2;
-	const int sh_id2=threadIdx.x +blockDim.x+ threadIdx.y*blockDim.x*2;
+	const int i = blockIdx[0]*blockDim[0]*2+threadIdx[0];
+	const int i2= blockIdx[0]*blockDim[0]*2+threadIdx[0]+blockDim[0];
+    const int j = blockIdx[1]*blockDim[1]+threadIdx[1];
+	const int sh_id=threadIdx[0] + threadIdx[1]*blockDim[0]*2;
+	const int sh_id2=threadIdx[0] +blockDim[0]+ threadIdx[1]*blockDim[0]*2;
 	
 	//shared memeory
-	extern __shared__ float sh_A0[];
+	tile_static float sh_A0[32*4*2];
 	sh_A0[sh_id]=0.0f;
 	sh_A0[sh_id2]=0.0f;
-	__syncthreads();
+    tidx.barrier.wait();
 	
 	//get available region for load and store
 	const bool w_region =  i>0 && j>0 &&(i<(nx-1)) &&(j<(ny-1)) ;
 	const bool w_region2 =  j>0 &&(i2<nx-1) &&(j<ny-1) ;
-	const bool x_l_bound = (threadIdx.x==0);
-	const bool x_h_bound = ((threadIdx.x+blockDim.x)==(blockDim.x*2-1));
-	const bool y_l_bound = (threadIdx.y==0);
-	const bool y_h_bound = (threadIdx.y==(blockDim.y-1));
+	const bool x_l_bound = (threadIdx[0]==0);
+	const bool x_h_bound = ((threadIdx[0]+blockDim[0])==(blockDim[0]*2-1));
+	const bool y_l_bound = (threadIdx[1]==0);
+	const bool y_h_bound = (threadIdx[1]==(blockDim[1]-1));
 
 	//register for bottom and top planes
 	//because of thread coarsening, we need to doulbe registers
@@ -50,7 +57,7 @@ __global__ void block2D_hybrid_coarsen_x(float c0,float c1,float *A0,float *Anex
 		sh_A0[sh_id2]=A0[Index3D (nx, ny, i2, j, 1)];
 	}
 
-	__syncthreads();
+    tidx.barrier.wait();
 	
 	for(int k=1;k<nz-1;k++)
 	{
@@ -65,8 +72,8 @@ __global__ void block2D_hybrid_coarsen_x(float c0,float c1,float *A0,float *Anex
 			
 		if(w_region)
 		{
-			a_up        =y_h_bound?A0[Index3D (nx, ny, i, j+1, k )]:sh_A0[sh_id+2*blockDim.x];
-      a_down      =y_l_bound?A0[Index3D (nx, ny, i, j-1, k )]:sh_A0[sh_id-2*blockDim.x];
+			a_up        =y_h_bound?A0[Index3D (nx, ny, i, j+1, k )]:sh_A0[sh_id+2*blockDim[0]];
+      a_down      =y_l_bound?A0[Index3D (nx, ny, i, j-1, k )]:sh_A0[sh_id-2*blockDim[0]];
 			a_left_right=x_l_bound?A0[Index3D (nx, ny, i-1, j, k )]:sh_A0[sh_id-1];
 	
 			Anext[Index3D (nx, ny, i, j, k)] = (top + bottom + a_up + a_down + sh_A0[sh_id+1] +a_left_right)*c1
@@ -80,8 +87,8 @@ __global__ void block2D_hybrid_coarsen_x(float c0,float c1,float *A0,float *Anex
 			
 		if(w_region2)
 		{
-		  a_up        =y_h_bound?A0[Index3D (nx, ny, i2, j+1, k )]:sh_A0[sh_id2+2*blockDim.x];
-      a_down      =y_l_bound?A0[Index3D (nx, ny, i2, j-1, k )]:sh_A0[sh_id2-2*blockDim.x];
+		  a_up        =y_h_bound?A0[Index3D (nx, ny, i2, j+1, k )]:sh_A0[sh_id2+2*blockDim[0]];
+      a_down      =y_l_bound?A0[Index3D (nx, ny, i2, j-1, k )]:sh_A0[sh_id2-2*blockDim[0]];
 			a_left_right=x_h_bound?A0[Index3D (nx, ny, i2+1, j, k )]:sh_A0[sh_id2+1];
 
 			
@@ -90,13 +97,13 @@ __global__ void block2D_hybrid_coarsen_x(float c0,float c1,float *A0,float *Anex
 		}
 
 		//swap data
-		__syncthreads();
-		bottom=sh_A0[sh_id];
-		sh_A0[sh_id]=top;
-		bottom2=sh_A0[sh_id2];
-		sh_A0[sh_id2]=top2;
-		__syncthreads();
-	}
+        tidx.barrier.wait();
+        bottom=sh_A0[sh_id];
+        sh_A0[sh_id]=top;
+        bottom2=sh_A0[sh_id2];
+        sh_A0[sh_id2]=top2;
+        tidx.barrier.wait();
+    }
 
 
 }

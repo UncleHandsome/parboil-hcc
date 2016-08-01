7f36f42a5ad3cfd44ae89e1eeb809dca5ce5c17d
UncleHandsome <yunwei@multicorewareinc.com>
2016-08-01 04:11:17 -0500

    [SGEMM] INIT

--- benchmarks/sgemm/src/hcc/main.cpp
+++ benchmarks/sgemm/src/hcc/main.cpp
@@ -27,15 +27,13 @@ using namespace hc;
 extern bool readColMajorMatrixFile(const char *fn, int &nr_row, int &nr_col, std::vector<float>&v);
 extern bool writeColMajorMatrixFile(const char *fn, int, int, std::vector<float>&);
 
-extern "C"
-void computeGold(float *, const float*, const float*, unsigned int, unsigned int, unsigned int);
-
 int
 main (int argc, char *argv[]) {
 
   struct pb_Parameters *params;
   struct pb_TimerSet timers;
 
+  float *dA, *dB, *dC;
   size_t A_sz, B_sz, C_sz;
   int matArow, matAcol;
   int matBrow, matBcol;
@@ -62,36 +60,38 @@ main (int argc, char *argv[]) {
   readColMajorMatrixFile(params->inpFiles[0],
       matArow, matAcol, matA);
   // copy A to device memory
-  A_sz = matArow*matAcol;
+  A_sz = matArow*matAcol*sizeof(float);
 
   // load B^T
   readColMajorMatrixFile(params->inpFiles[2],
       matBcol, matBrow, matBT);
 
   pb_SwitchToTimer( &timers, pb_TimerID_COMPUTE );
-  B_sz = matBrow*matBcol;
+  B_sz = matBrow*matBcol*sizeof(float);
 
   // allocate space for C
-  C_sz = matArow*matBcol;
+  C_sz = matArow*matBcol*sizeof(float);
 
-  // CUDA memory allocation
+  // HCC memory allocation
   std::vector<float> matC(matArow*matBcol);
-  array_view<const float> dA(A_sz, matA);
-  array_view<const float> dB(B_sz, matBT);
-  array_view<float> dC(C_sz, matC);
+  cudaMalloc((void**)&dA, A_sz);
+  cudaMalloc((void**)&dB, B_sz);
+  cudaMalloc((void**)&dC, C_sz);
 
   // Copy A and B^T into device memory
   pb_SwitchToTimer( &timers, pb_TimerID_COPY );
+  cudaMemcpy(dA, &matA.front(), A_sz, cudaMemcpyHostToDevice);
+  cudaMemcpy(dB, &matBT.front(), B_sz, cudaMemcpyHostToDevice);
 
   pb_SwitchToTimer( &timers, pb_TimerID_KERNEL );
 
   // Use standard sgemm interface
-  basicSgemm('N', 'T', matArow, matBcol, matAcol, 1.0f, \
+  regtileSgemm('N', 'T', matArow, matBcol, matAcol, 1.0f, \
       dA, matArow, dB, matBcol, 0.0f, dC, matArow);
 
   if (params->outFile) {
     pb_SwitchToTimer( &timers, pb_TimerID_COPY );
-    dC.synchronize();
+    cudaMemcpy(&matC.front(), dC, C_sz, cudaMemcpyDeviceToHost);
     /* Write C to file */
     pb_SwitchToTimer(&timers, pb_TimerID_IO);
     writeColMajorMatrixFile(params->outFile,
@@ -104,5 +104,8 @@ main (int argc, char *argv[]) {
   std::cout<< "GFLOPs = " << 2.* matArow * matBcol * matAcol/GPUtime/1e9 << std::endl;
   pb_PrintTimerSet(&timers);
   pb_FreeParameters(params);
+  cudaFree(dA);
+  cudaFree(dB);
+  cudaFree(dC);
   return 0;
 }
--- benchmarks/sgemm/src/hcc_base/sgemm_kernel.hpp
+++ benchmarks/sgemm/src/hcc_base/sgemm_kernel.hpp
@@ -14,11 +14,16 @@
 // Parameters of tile sizes
 #define TILE_SZ 16
 
-__global__ void mysgemmNT( const float *A, int lda, const float *B, int ldb, float* C, int ldc, int k, float alpha, float beta )
+void mysgemmNT(
+        tiled_index<2>& tidx,
+        const array_view<const float>& A, int lda,
+        const array_view<const float>& B, int ldb,
+        const array_view<float>& C, int ldc,
+        int k, float alpha, float beta ) [[hc]]
 {
     float c = 0.0f;
-    int m = blockIdx.x * blockDim.x + threadIdx.x;
-    int n = blockIdx.y * blockDim.y + threadIdx.y;
+    int m = tidx.global[0];
+    int n = tidx.global[1];
     for (int i = 0; i < k; ++i) {
 	float a = A[m + i * lda];
 	float b = B[n + i * ldb];
@@ -27,7 +32,10 @@ __global__ void mysgemmNT( const float *A, int lda, const float *B, int ldb, flo
     C[m+n*ldc] = C[m+n*ldc] * beta + alpha * c;
 }
 
-void basicSgemm( char transa, char transb, int m, int n, int k, float alpha, const float *A, int lda, const float *B, int ldb, float beta, float *C, int ldc )
+void basicSgemm( char transa, char transb, int m, int n, int k, float alpha,
+        array_view<const float>& A, int lda,
+        array_view<const float>& B, int ldb, float beta,
+        array_view<float>& C, int ldc )
 {
   if ((transa != 'N') && (transa != 'n')) {
     std::cerr << "unsupported value of 'transa' in regtileSgemm()" << std::endl;
@@ -46,9 +54,12 @@ void basicSgemm( char transa, char transb, int m, int n, int k, float alpha, con
   }
 
 
-  dim3 grid( m/TILE_SZ, n/TILE_SZ ), threads( TILE_SZ, TILE_SZ );
-  mysgemmNT<<<grid, threads>>>( A, lda, B, ldb, C, ldc, k, alpha, beta);
-  CHECK_ERROR("mySgemm");
-
+  int threads[2] = {TILE_SZ, TILE_SZ};
+  int grid[2] = {m/TILE_SZ*threads[0], n/TILE_SZ*threads[1]};
+  parallel_for_each(extent<2>(grid).tile(threads[0], threads[1]),
+          [=] (tiled_index<2> tidx) [[hc]]
+          {
+          mysgemmNT(tidx, A, lda, B, ldb, C, ldc, k, alpha, beta);
+          });
 }
 
